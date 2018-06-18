5504bd9bd298a5eee085136ed70301e4bb912489
JkayZD <38204635+JkayZD@users.noreply.github.com>
2018-06-18 18:17:03 +0800

    Add files via upload

---
+++ js/jkay-artistry-javascript.js
@@ -0,0 +1,14 @@
+
+// $(document).ready(function({
+//   $(window).resize(function(){
+//   if($(window).width() > 500){
+//     $('ul').removeAttr('style');
+//     }
+//   });
+
+
+
+$(".showreelbtn").click(function () {
+  console.log("ppp");
+
+});  
